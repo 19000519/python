7e310b0d3eb4af27c1050809cb48f97d5ad2ff14
KOSWANE PHEMELO <KP19000519@BIUST.AC.BW>
2020-11-05 23:20:19 +0200

    NW

---
+++ new.py
@@ -0,0 +1,13 @@
+#Author : Phenyo
+#Purpose : validate length of a string
+
+weight = float(input('Weight : '))
+unit = input('(L)bs or (K)g : ')
+
+if unit.upper() == "L" :
+    converted = weight * 0.453
+    print(f'you are {converted} kilograms')
+
+elif unit.upper() == "K":
+    converted = weight / 0.453
+    print(f'you are {converted} pounds')
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+    print(f"you are {weight} pounds")
